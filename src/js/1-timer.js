65774b4205b494970a718ab5e1e6d532f1892efd
KL-ML <knataly0513@gmail.com>
2025-03-28 12:25:23 +0200

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -22,8 +22,7 @@ flatpickr(datetimeInput, {
         position: 'topRight',
         backgroundColor: 'rgba(239, 64, 64, 0.99)',
         messageColor: '#FFFFFF',
-        iconUrl: '/img/bi_x-octagon.svg',
-        iconColor: '#FFFFFF',
+        icon: '',
         close: false,
       });
       return;
